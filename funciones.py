2aa041d60768891852dfe9a04debb9dfa9d0e4b1
Pacott1 <f.delascuevasvalencia@gmail.com>
2025-11-17 12:23:48 +0100

    .

--- funciones.py
+++ funciones.py
@@ -177,3 +177,251 @@ if __name__ == "__main__":
     print()
 
 
+def flota_peq_aleatorio():
+    # Dimensiones de tablero:
+    tablero_B = np.full((10,10), ' ')
+    filas_totales = tablero_B.shape[0]
+    columnas_totales = tablero_B.shape[1] 
+    # Fijamos unos numeros random que tendrá la computadora.
+    random.seed(42)
+
+    flota_peq = [] 
+    ocupadas = set() # Set para guardar las posiciones y que nos servira a futuro de comprobar que no estemos poniendo en la misma posicion 2 barcos.
+
+    for barco in range(4):
+        while True:
+            fila = random.randint(0,filas_totales-1)
+            columna = random.randint(0,columnas_totales-1)
+            primera_pieza = (fila,columna)
+
+            if primera_pieza in ocupadas:
+                continue
+
+            flota_peq.append(primera_pieza)
+            ocupadas.add(primera_pieza)
+            tablero_B[primera_pieza] = 'P'
+
+            break
+
+    return flota_peq, tablero_B
+
+flota_peq, tablero_B = flota_peq_aleatorio()
+
+
+
+
+def flota_med_aleatoria(tablero, flota_peq):
+    filas_totales = tablero_B.shape[0]
+    columnas_totales = tablero_B.shape[1] 
+    
+    ocupadas = set(flota_peq)
+    flota_med=[]
+    random.seed(42)
+    for barco in range(3):
+        while True:
+        # Primera pieza:
+            fila = random.randint(0,filas_totales-1)
+            columna = random.randint(0,columnas_totales-1)
+            primera_pieza = (fila,columna)
+        # Si la primera pieza es una de las ocupadas, vuelve a sacar una primera pieza
+            if primera_pieza in ocupadas:
+                continue
+        # Elegimos orientacion, para asegurarnos que esten juntas las coordenadas de los barcos medianos.            
+            orientacion = random.choice(['N','S','O','E'])
+
+            fila2,columna2 = fila,columna # Muy importante ya que le decimos que la fila y columna de la segunda pieza en principio sea igual que la primera pieza y en funcion de la coordenad, sumamos o restamos columna o fila.
+
+            match orientacion:
+                case 'N':
+                    fila2 = fila - 1
+                                
+                case 'S':
+                    fila2 = fila + 1
+                   
+                case 'O':
+                   columna2 = columna - 1
+                    
+                case 'E':
+                    columna2 = columna + 1
+                   
+            if not (0 <= fila2 < filas_totales and 0 <= columna2 < columnas_totales):
+                continue # Si la fila y columna no es mayor que las totlas y no son menores que 0, agregala a la variable segunda_pieza
+            
+            segunda_pieza = (fila2,columna2)
+
+            if segunda_pieza in ocupadas:
+                continue
+
+            flota_med.append([primera_pieza,segunda_pieza])
+            ocupadas.add(primera_pieza)
+            ocupadas.add(segunda_pieza)
+
+            tablero[primera_pieza] = 'M'
+            tablero[segunda_pieza] = 'M'
+
+
+
+            break
+    return flota_med,tablero
+
+flota_med, tablero_B = flota_med_aleatoria(tablero_B,flota_peq) # Guardo asi la variable ya que la funcion me da un lista: [(flota_med), tablero_B]
+
+
+
+def flota_grand_aleatoria(tablero, flota_peq,flota_med):
+    filas_totales = tablero_B.shape[0]
+    columnas_totales = tablero_B.shape[1] 
+
+    ocupadas = set(flota_peq) 
+    for barco in flota_med:
+        for pieza in barco:
+            ocupadas.add(pieza)
+    flota_grand = []
+    random.seed(42)
+    for barco in range(2):
+        while True:
+        # Primera pieza:
+            fila = random.randint(0,filas_totales-1)
+            columna = random.randint(0,columnas_totales-1)
+            primera_pieza = (fila,columna)
+        # Si la primera pieza es una de las ocupadas, vuelve a sacar una primera pieza
+            if primera_pieza in ocupadas:
+                continue
+        # Elegimos orientacion, para asegurarnos que esten juntas las coordenadas de los barcos medianos.            
+            orientacion = random.choice(['N','S','O','E'])
+
+            fila2,columna2 = fila,columna
+            fila3,columna3 = fila,columna  # Muy importante ya que le decimos que la fila y columna de la segunda pieza en principio sea igual que la primera pieza y en funcion de la coordenad, sumamos o restamos columna o fila.
+            
+            match orientacion:
+                case 'N':
+                    fila2 = fila -1 
+                    fila3 = fila -2            
+                case 'S':
+                   fila2 = fila + 1
+                   fila3 = fila + 2
+                case 'O':
+                   columna2 = columna - 1
+                   columna3 = columna - 2
+                case 'E':
+                    columna2 = columna + 1
+                    columna3 = columna + 2
+
+
+            if not (0 <= fila2 < filas_totales and 0 <= columna2 < columnas_totales):
+                continue # Si la fila y columna no es mayor que las totlas y no son menores que 0, agregala a la variable segunda_pieza
+
+            if not (0 <= fila3 < filas_totales and 0 <= columna3 < columnas_totales):
+                continue # Si la fila y columna no es mayor que las totaless y no son menores que 0, agregala a la variable tercera_pieza
+            segunda_pieza = (fila2,columna2)
+            tercera_pieza = (fila3,columna3)
+
+            if segunda_pieza in ocupadas:
+                continue
+            
+            if tercera_pieza in ocupadas: # Compruebo que la tercera pieza no este ocupada antes.
+                continue
+
+            flota_grand.append([primera_pieza,segunda_pieza,tercera_pieza])
+            ocupadas.add(primera_pieza)
+            ocupadas.add(segunda_pieza)
+            ocupadas.add(tercera_pieza)
+
+            tablero[primera_pieza] = 'G'
+            tablero[segunda_pieza] = 'G'
+            tablero[tercera_pieza] = 'G'
+
+
+            break
+    return flota_grand,tablero
+
+flota_grand, tablero_B = flota_grand_aleatoria(tablero_B,flota_peq,flota_med)
+
+
+
+def flota_enorme_aleatoria(tablero, flota_peq,flota_med,flota_grand):
+    filas_totales = tablero_B.shape[0]
+    columnas_totales = tablero_B.shape[1] 
+
+    ocupadas = set(flota_peq) 
+    for barco in flota_med:
+        for pieza in barco:
+            ocupadas.add(pieza)
+    for barco in flota_grand:
+        for pieza in barco:
+            ocupadas.add(pieza)
+    flota_enorme = []
+    random.seed(42)
+    for barco in range(1):
+        while True:
+        # Primera pieza:
+            fila = random.randint(0,filas_totales-1)
+            columna = random.randint(0,columnas_totales-1)
+            primera_pieza = (fila,columna)
+        # Si la primera pieza es una de las ocupadas, vuelve a sacar una primera pieza
+            if primera_pieza in ocupadas:
+                continue
+        # Elegimos orientacion, para asegurarnos que esten juntas las coordenadas de los barcos medianos.            
+            orientacion = random.choice(['N','S','O','E'])
+
+            fila2,columna2 = fila,columna # Muy importante ya que le decimos que la fila y columna de la segunda pieza en principio sea igual que la primera pieza y en funcion de la coordenad, sumamos o restamos columna o fila.
+            fila3,columna3 = fila,columna
+            fila4,columna4 = fila,columna
+            match orientacion:
+                case 'N':
+                    fila2 = fila - 1
+                    fila3 = fila - 2 
+                    fila4 = fila - 3           
+                case 'S':
+                   fila2 = fila + 1
+                   fila3 = fila + 2
+                   fila4 = fila + 3
+                case 'O':
+                   columna2 = columna - 1
+                   columna3 = columna - 2
+                   columna4 = columna - 3
+                case 'E':
+                    columna2 = columna + 1
+                    columna3 = columna + 2
+                    columna4 = columna + 3
+
+            if not (0 <= fila2 < filas_totales and 0 <= columna2 < columnas_totales):
+                continue # Si la fila y columna no es mayor que las totaless y no son menores que 0, agregala a la variable segunda_pieza
+            if not (0 <= fila3 < filas_totales and 0 <= columna3 < columnas_totales):
+                continue # Si la fila y columna no es mayor que las totaless y no son menores que 0, agregala a la variable tercera_pieza
+            if not (0 <= fila4 < filas_totales and 0 <= columna4 < columnas_totales):
+                continue # Si la fila y columna no es mayor que las totaless y no son menores que 0, agregala a la variable tercera_pieza
+
+
+            segunda_pieza = (fila2,columna2)
+            tercera_pieza = (fila3,columna3)
+            cuarta_pieza = (fila4,columna4)
+
+
+            if segunda_pieza in ocupadas:
+                continue
+            if tercera_pieza in ocupadas: # Compruebo que la tercera pieza no este ocupada antes.
+                continue
+            if cuarta_pieza in ocupadas: # Compruebo que la tercera pieza no este ocupada antes.
+                continue
+
+            
+
+            flota_enorme.append([primera_pieza,segunda_pieza,tercera_pieza,cuarta_pieza])
+            ocupadas.add(primera_pieza)
+            ocupadas.add(segunda_pieza)
+            ocupadas.add(tercera_pieza)
+            ocupadas.add(cuarta_pieza)
+
+            tablero[primera_pieza] = 'E'
+            tablero[segunda_pieza] = 'E'
+            tablero[tercera_pieza] = 'E'
+            tablero[cuarta_pieza] = 'E'
+
+
+            break
+    return flota_med,tablero
+
+flota_grand, tablero_B = flota_enorme_aleatoria(tablero_B,flota_peq,flota_med,flota_grand)
+
+
